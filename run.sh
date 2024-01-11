68069292744f8b31d071fe3a79d8801123ce5bd9
AbdouTlili <ha_tlili@esi.dz>
2024-01-11 21:33:11 +0100

    test ci v2

--- run.sh
+++ run.sh
@@ -18,12 +18,7 @@ dblogs() {
 }
 
 initdb() {
-    if [[ -n "$1" ]]; then
-        echo "Initializing database with name: $1"
-        docker exec -it go_postgres psql -U postgres -c "create database $1" # db-name
-    else
-        echo "No database name provided, use initdb <database-name>"
-    fi
+    docker exec -it go_postgres psql -U postgres -c products_db
 }
 
 cleanup() {
